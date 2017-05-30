f159cea1c87525d1b4c12a1cdcf978a001d238e2
Samantha  Lambert <samanthalambert@Samanthas-MacBook-Pro-7.local>
2017-05-30 11:57:42 -0400

    final

---
+++ src/main/java/EX3.java
@@ -0,0 +1,51 @@
+package sal162;
+
+
+
+public class EX3 {
+
+    public static long lazy(long c) {
+
+    	return (c * c+ c +2)/2;
+
+    }
+    public static long tri(long c) {
+
+    	return c * (c + 1)/2;
+
+    }
+
+    
+    public static void main(String[] args) {
+
+    	
+    	
+    	if (args.length != 2)
+    	{
+    		System.out.println("Incorrect number of inputs");
+    		System.exit(3);
+    	}
+    	int c = 0;
+    	try{
+		c = Integer.parseInt(args[1]);
+	    } catch (NumberFormatException ex) {
+		System.out.println("Enter an integer for the second arg");
+		System.exit(2);
+	    }
+
+	    if (args[0] == "lazy")
+	    {
+	    	System.out.println("lazy(" + c + ") = " + lazy(c));
+	    }
+	    else if (args[0] == "tri")
+	    {
+	    	System.out.println("tri(" + c + ") = " + tri(c));
+	    }
+	    else
+	    {
+	    	System.out.println("First arg should be 'lazy' or 'tri'");	
+	    	System.exit(1);
+	    }
+
+
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import sal162.EX3;
+
+public class EX3Test {
+
+    @Test
+    public void testTri0() {
+	assertEquals(EX3.tri(0), 1);
+    }
+
+    
+    @Test
+    public void testTri1() {
+	assertEquals(EX3.tri(1), 1);
+    }
+
+    @Test
+    public void testTri4() {
+	assertEquals(EX3.tri(4), 10);
+    }
+    
+
+    @Test
+    public void testLazy0() {
+    assertEquals(EX3.lazy(0), 1);
+    }
+
+    
+    @Test
+    public void testLazy1() {
+    assertEquals(EX3.caterer(1), 2);
+    }
+
+    @Test
+    public void testLazy4() {
+    assertEquals(EX3.lazy(4), 11);
+    }
+    
+    
+}
